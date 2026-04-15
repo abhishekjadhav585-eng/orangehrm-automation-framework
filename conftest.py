b9dd2e9a3c52580d445910d968a8fa5b882bc13a
abhishek jadhav <abhishekjadhav585@gmail.com>
2026-04-15 18:20:48 +0530

    allure report support added

--- conftest.py
+++ conftest.py
@@ -1,8 +1,27 @@
 import pytest
+import allure
+from allure_commons.types import AttachmentType
 from drivers.driver_factory import get_driver
+
 
 @pytest.fixture
 def driver():
     driver = get_driver()
     yield driver
     driver.quit()
+
+
+# Hook: Capture screenshot on test failure
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="Failure Screenshot",
+                attachment_type=AttachmentType.PNG
+            )
